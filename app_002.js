8618b89f200914dbbdcafaa8c68f1f535b43f0b0
KenoLeon <me@k3no.com>
2018-02-26 19:29:07 -0600

    app 002

--- app_002.js
+++ app_002.js
@@ -4,21 +4,15 @@ $("#getCurrencies").click(function() {
 
 
 function getCurrencies() {
-
+  $(".data").empty(); // clear Data
   // call API:
   // /v1/market/currencies [GET]
-
   axios.get('https://api.cobinhood.com/v1/market/currencies')
-    $(".data").empty(); // clear Data
-    // Return Promise
     .then(function(response) {
-
+      // dump into data container...
       $(".data").append("<div class='card bg-dark mb-3'><div class='card-body'>" + JSON.stringify(response) + "</div></div>");
-
     })
-    // or Catch error
     .catch(function(error) {
       console.log(errror);
     });
-
 }

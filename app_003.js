53f633219f8bb3ca489a75827ea6188bf9ef7fb4
KenoLeon <me@k3no.com>
2018-02-27 10:49:41 -0600

    app_003

---
+++ app_003.js
@@ -0,0 +1,18 @@
+$("#getCurrencies").click(function() {
+  getCurrencies();
+});
+
+function getCurrencies() {
+  $(".data").empty();
+  axios.get('https://api.cobinhood.com/v1/market/currencies')
+    .then(function(response) {
+      // go over each item in currencies array...
+      $.each(response.data.result.currencies, function(index, value){
+          console.log(value.currency);
+          $(".data").append("<p><b>"+value.currency+ "</b>&nbsp;"+ value.name+"</p>")
+      });
+    })
+    .catch(function(error) {
+      console.log(errror);
+    });
+}
